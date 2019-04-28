6caf49f6914187c7df5359dd31368370b7372345
hyang <1348261663@qq.com>
2019-04-28 22:31:40 +0800

    Update NodeJS中的this.js

--- NodeJS中的this.js
+++ NodeJS中的this.js
@@ -3,12 +3,11 @@
 
 //在全局中的this
 
-
 console.log(this); {}
 this.num = 10;
 console.log(this.num); 10
 console.log(global.num); undefined
-//全局中的this默认是一个空对象。并且在全局中this与global对象没有任何的关系，那么全局中的this究竟指向的是谁？在本章节后半部分我们会讲解。
+//全局中的this默认是一个空对象。并且在全局中this与global对象没有任何的关系
 
 在函数中的this
 
